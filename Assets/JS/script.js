9258189b4209d9b55a4ca5f152bed3a5192ec59a
Abewd <116496332+abewd@users.noreply.github.com>
2022-12-31 13:35:25 +1100

    js code complete, just missing comments

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -21,3 +21,20 @@ function setTimeSlotColors() {
     }
   }
 }
+
+// Display saved tasks which have been previously saved
+function getStoredTasks() {
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  $(".display-text").each(function (index, element) {
+    for (var i = 0; i < tasks.length; i++) {
+      if (tasks[i].pID == element.id) {
+        element.textContent = tasks[i].task;
+      }
+    }
+  });
+}
